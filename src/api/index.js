fa2495a8c6ac1d4b14c773473c3c5f317fb59f74
hooray <304327508@qq.com>
2020-09-29 10:42:12 +0800

    调整axiso拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -57,21 +57,22 @@ api.interceptors.response.use(
     response => {
         /**
          * 全局拦截请求发送后返回的数据，如果数据有报错则在这做全局的错误提示
-         * 假设返回数据格式为：{ code: 200, error: '', data: '' }
-         * 规则是当 code 为 200 时表示请求成功，非 200 时的其它值均为请求失败，并且会在 error 里返回失败提示信息
-         * 其中 code 如果为 0 表示接口需要登录或者登录状态失效，需要重新登录
+         * 假设返回数据格式为：{ status: 1, error: '', data: '' }
+         * 规则是当 status 为 1 时表示请求成功，为 0 时表示接口需要登录或者登录状态失效，需要重新登录
+         * 请求出错时 error 会返回错误信息
          * 则代码如下
          */
-        if (response.data.code !== 200) {
-            if (response.data.code === 0) {
-                toLogin()
+        if (response.data.status === 1) {
+            if (response.data.error === '') {
+                // 请求成功并且没有报错
+                return Promise.resolve(response.data)
             } else {
                 // 这里做错误提示，如果使用了 element ui 则可以使用 Message 进行提示
                 // Message.error(options)
                 return Promise.reject(response.data)
             }
         } else {
-            return Promise.resolve(response.data)
+            toLogin()
         }
     },
     error => {
